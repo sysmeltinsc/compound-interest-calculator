64e88d80a739e901e264cb3e7cadd98fc30ecdc4
sysmeltinsc <testantigravity218@gmail.com>
2026-01-11 20:21:21 +0000

    feat: add Google Analytics (react-ga4)

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize("G-90N75SGNF9");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
